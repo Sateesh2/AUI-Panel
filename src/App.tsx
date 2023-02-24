51719fe95765b5002fd265ecad12a912ccf1f1ec
Sateesh <sateesh@Sateeshs-MacBook-Pro.local>
2023-02-24 15:55:15 +0530

    added basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,69 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import styled, {createGlobalStyle} from 'styled-components';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='wrapper'>
+        <GlobalStyles />
+        <div style={{ height: "100%", width: '20%' }}></div>
+        <ContentDiv>
+          <div>
+            <a href="https://vitejs.dev" target="_blank">
+              <img src="/vite.svg" className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://reactjs.org" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </div>
+          <h1>Vite + React</h1>
+          <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </button>
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+        </ContentDiv>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
+
   )
 }
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    --color-text: black;
+    --color-background: white;
+    --color-primary: #66339980;
+    --main-container-width: 80%;
+  }
+`;
+
+const MenuDiv = styled.div`
+`;
+
+const ContentDiv = styled.div`
+  position: absolute;
+  height: 100%;
+  width: var(--main-container-width);
+  left: calc(100vw - var(--main-container-width));
+
+
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: #66339980;
+`;
+
 
 export default App
