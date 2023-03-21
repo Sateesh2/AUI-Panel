761c26886ca755aeea386357c14a0d2769d7f6aa
Sateesh <sateesh@Sateeshs-MacBook-Pro.local>
2023-03-22 00:19:24 +0530

    added drag moving logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,48 @@ import './App.css'
 import styled, {createGlobalStyle} from 'styled-components';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const startX = e.clientX;
+    const cssVar = getComputedStyle(document.body).getPropertyValue('--main-container-width');
+    console.log(cssVar);
+ 
+    let startWidth = 0;
+    if (cssVar.includes('%')) {
+      startWidth = parseInt(cssVar, 10) * window.innerWidth / 100;
+
+      console.log(startWidth);
+    } else {
+      startWidth = parseInt(cssVar, 10);
+
+      console.log(startWidth);
+    }
+
+
+    const handleMouseMove = (e: MouseEvent) => {
+
+      const newWidth = startWidth + (startX - e.clientX);
+      document.body.style.setProperty('--main-container-width', `${newWidth}px`);
+    };
+
+    const handleMouseUp = () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseup', handleMouseUp);
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+  };
+
 
   return (
     <div className="App">
       <div className='wrapper'>
         <GlobalStyles />
-        <div style={{ height: "100%", width: '20%' }}></div>
+        <div style={{ height: "100%", width: '20%' }}>
+          <DragHandle onMouseDown={handleMouseDown}/>
+        </div>
         <ContentDiv>
           <div>
             <a href="https://vitejs.dev" target="_blank">
@@ -49,6 +84,22 @@ const GlobalStyles = createGlobalStyle`
 `;
 
 const MenuDiv = styled.div`
+`;
+
+const DragHandle = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 20px;
+  left: calc(100vw - var(--main-container-width) - 20px);
+  background: #66339980;
+
+  cursor: ew-resize;
+
+  &:hover {
+    background: #663399;
+
+    border-left: 2px solid blue;
+  }
 `;
 
 const ContentDiv = styled.div`
